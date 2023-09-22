5aadd4616b58fbe165727ae9e6270403d3a83df8
yiphei <yanyiphei@gmail.com>
2023-09-21 17:40:15 -0700

    added changes

--- value.py
+++ value.py
@@ -20,6 +20,19 @@ class Value(object):
         
         return out
     
+    def __relu__(self):
+        out  = Value(self.scalar if self.scalar >= 0 else 0, prevs=[self])
+        self.compute_gradient = lambda last_grad: last_grad if self.scalar >= 0 else 0
+
+        return out
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Value(self.scalar ** other, prevs=[self])
+
+        self.compute_gradient = lambda last_grad: last_grad * (other * self.scalar ** (other - 1))
+        return out
+    
     def __mul__(self,other):
         other = self.cast_to_value(other)
         out = Value(self.scalar * other.scalar, prevs=[self, other])
@@ -28,6 +41,21 @@ class Value(object):
         other.compute_gradient = lambda last_grad: last_grad * self.scalar
 
         return out
+    
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self,other):
+        return self + (-other)
+    
+    def __rsub__(self,other):
+        return other + (-self)
+    
+    def __truediv__(self,other):
+        return self * other ** -1
+    
+    def __rtruediv__(self,other):
+        return other / self
     
     def backward(self, last_grad = None):
         if last_grad is None:
