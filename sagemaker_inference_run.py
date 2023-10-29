5b5ba4bc983da43487458fdb1735ed70f010e8c4
yiphei <yanyiphei@gmail.com>
2023-10-29 11:26:30 -0700

    added changes

--- sagemaker_inference_run.py
+++ sagemaker_inference_run.py
@@ -1,17 +1,32 @@
 import sagemaker
 from sagemaker.pytorch import PyTorchModel
 from sagemaker.predictor import Predictor
+import boto3
 
 # Initialize the SageMaker session and role
-sagemaker_session = sagemaker.Session()
-role = 's3://sagemaker-studio-mk6unewb9tb/pytorch-training-2023-10-29-17-41-10-561/output/model.tar.gz'  # replace with your SageMaker role ARN
+role = 'arn:aws:iam::252201027045:role/service-role/SageMaker-training-training-ML-2'  # replace with your SageMaker role ARN
+
+my_region = 'us-east-1'  # change to your desired region
+
+# Creating the sagemaker client using boto3
+sagemaker_client = boto3.client('sagemaker', region_name=my_region)
+sagemaker_runtime_client = boto3.client('sagemaker-runtime', region_name=my_region)
+
+# Correcting the default bucket name, it shouldn't be a full S3 path
+default_bucket = 'sagemaker-studio-mk6unewb9tb'  # Ensure this is your correct S3 bucket name
+
+sagemaker_session = sagemaker.Session(default_bucket=default_bucket,
+                                      sagemaker_client=sagemaker_client,
+                                      sagemaker_runtime_client=sagemaker_runtime_client)
+
 
 # Create a PyTorch model from the trained artifact
 model_artifact = 's3://sagemaker-studio-mk6unewb9tb/pytorch-training-2023-10-29-04-49-30-583/output/model.tar.gz'  # replace with the path to your trained model artifact
 pytorch_model = PyTorchModel(model_data=model_artifact,
                              role=role,
                              framework_version='1.9.0',  # replace with your PyTorch version
-                             entry_point='inference.py',  # replace with your inference script
+                             py_version='py38',
+                             entry_point='sagemaker_inference.py',  # replace with your inference script
                              sagemaker_session=sagemaker_session)
 
 # Deploy the model for asynchronous inference
