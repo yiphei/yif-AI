04b73361f5be083adabdf21ba3adc6becb5624d8
yiphei <yanyiphei@gmail.com>
2023-10-29 11:17:22 -0700

    added changes

--- sagemaker_inference_run.py
+++ sagemaker_inference_run.py
@@ -10,7 +10,7 @@ role = 'arn:aws:iam::252201027045:role/service-role/SageMaker-training-training-
 model_artifact = 's3://sagemaker-studio-mk6unewb9tb/pytorch-training-2023-10-29-04-49-30-583/output/model.tar.gz'  # replace with the path to your trained model artifact
 pytorch_model = PyTorchModel(model_data=model_artifact,
                              role=role,
-                             framework_version='1.8.0',  # replace with your PyTorch version
+                             framework_version='1.9.0',  # replace with your PyTorch version
                              entry_point='inference.py',  # replace with your inference script
                              sagemaker_session=sagemaker_session)
 
@@ -18,5 +18,4 @@ pytorch_model = PyTorchModel(model_data=model_artifact,
 predictor = pytorch_model.deploy(initial_instance_count=1,
                                  instance_type='ml.p3.8xlarge',
                                  endpoint_name='async-inference-endpoint',
-                                 inference_response_keys=['predicted_label'],
-                                 wait=True)
+                                 asynchronous=True)
