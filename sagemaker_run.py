6e7ffb6212412081cf009e0066419a7c598352bd
yiphei <yanyiphei@gmail.com>
2023-10-28 20:30:57 -0700

    added changes

--- sagemaker_run.py
+++ sagemaker_run.py
@@ -1,14 +1,31 @@
 import sagemaker
 from sagemaker.pytorch import PyTorch
 from sagemaker import get_execution_role
+import boto3
 
-role = get_execution_role()
+role = "arn:aws:iam::252201027045:role/service-role/SageMaker-training-training-ML-2"
 
-pytorch_estimator = PyTorch(entry_point='sagemaker_script.py', # the name of your script
+my_region = 'us-west-2'  # change to your desired region
+
+# Creating the sagemaker client using boto3
+sagemaker_client = boto3.client('sagemaker', region_name=my_region)
+sagemaker_runtime_client = boto3.client('sagemaker-runtime', region_name=my_region)
+
+# Correcting the default bucket name, it shouldn't be a full S3 path
+default_bucket = 'sagemaker-studio-mk6unegrfwb9tb'  # Ensure this is your correct S3 bucket name
+
+sagemaker_session = sagemaker.Session(default_bucket=default_bucket,
+                                      sagemaker_client=sagemaker_client,
+                                      sagemaker_runtime_client=sagemaker_runtime_client)
+
+pytorch_estimator = PyTorch(
+                            sagemaker_session=sagemaker_session,
+                            entry_point='sagemaker_script.py', # the name of your script
                             role=role,
                             framework_version='1.8.1', # select your PyTorch version
                             instance_count=1,
                             instance_type='ml.p3.8xlarge', # choose a suitable instance type
+                            py_version='py3',
                             hyperparameters={
                                 'batch_size': 64,
                                 'block_size': 256,
