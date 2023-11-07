2cdf8ff58a09c0758b7d8ef2abd4cd9ab66270ca
yiphei <yanyiphei@gmail.com>
2023-11-07 09:26:45 -0800

    added inference logging update

--- inference.py
+++ inference.py
@@ -1,5 +1,5 @@
 import logging
-logger = logging.getLogger()
+logger = logging.getLogger('__name__')
 logging.basicConfig(level=logging.INFO, 
                     format='YY %(asctime)s [%(levelname)s]: %(message)s')
 logger.setLevel(logging.INFO) 
