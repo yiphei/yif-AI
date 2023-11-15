849820f2d7635799777b0bfd28bedf4b89c34842
yiphei <yanyiphei@gmail.com>
2023-11-14 22:45:33 -0800

    added changes

--- tsetlin/tsetlin.py
+++ tsetlin/tsetlin.py
@@ -3,10 +3,10 @@ import random
 
 class TsetlinBase:
     def conjunction_mul(self, X, W):
-        matrix_X = X.repeat(W.shape[0], 1)
+        matrix_X = X.repeat(1, W.shape[0], 1)
         mask = W > 0 # TODO: Right now this is not a problem because whenever I make update W or X, the negation is always zero. But if I change that, I will prob need to compare and choose the clause with the highest weight
         masked_X = torch.where(mask, matrix_X, torch.tensor(1)) # theoretically, you should not replace it with 1 (it should just be omitted), but mathematically it works out fine because an extra 1 does not change the output of the multiplication
-        return torch.prod(masked_X, dim=1, keepdim=True).view(1,-1)
+        return torch.prod(masked_X, dim=2, keepdim=True).view(X.shape[0],-1)
 
 class TsetlinLayer(TsetlinBase):
     def __init__(self, in_dim, out_dim):
@@ -24,9 +24,8 @@ class TsetlinLayer(TsetlinBase):
 
     def forward(self, X):
         X_neg = 1 - X
-        self.full_X = torch.cat((X, X_neg), dim=0)
-        out = self.conjunction_mul(self.full_X.unsqueeze(0), self.W)
-        self.out = out.squeeze(0)
+        self.full_X = torch.cat((X, X_neg), dim=1)
+        self.out = self.conjunction_mul(self.full_X.unsqueeze(1), self.W)
         return self.out
     
     def helper(self, expected_X,update_idx, expected_W, can_flip_value, can_remove, can_add_value):
@@ -55,48 +54,53 @@ class TsetlinLayer(TsetlinBase):
                 expected_W[negation_idx] = 1
 
     def update(self, Y, is_first_layer = False):
-        can_flip_value = not (is_first_layer or torch.equal(Y, self.out))
-        if can_flip_value:
-            one_Y_idxs = torch.nonzero(Y == 1).squeeze(1)
-            W_halves = torch.split(self.W[one_Y_idxs], self.in_dim, dim=1)
-            pos_W = W_halves[0]
-            neg_W = W_halves[1]
-            for w_1 in pos_W:
-                idxs = torch.nonzero(w_1 == 1).squeeze(1)
-                if any((w_1[idxs] == w_2[idxs]).any() for w_2 in neg_W):
-                    can_flip_value = False
-                    break
+        can_flip_value = torch.full((Y.shape[0],), False)
+        if not is_first_layer:
+            for i, (y_value, out_value) in enumerate(zip(Y, self.out)):
+                if not torch.equal(y_value, out_value):
+                    can_flip_value[i] = True
+
+                    one_Y_idxs = torch.nonzero(y_value == 1).squeeze(1)
+                    W_halves = torch.split(self.W[one_Y_idxs], self.in_dim, dim=1)
+                    pos_W = W_halves[0]
+                    neg_W = W_halves[1]
+                    for w_1 in pos_W:
+                        idxs = torch.nonzero(w_1 == 1).squeeze(1)
+                        if any((w_1[idxs] == w_2[idxs]).any() for w_2 in neg_W):
+                            can_flip_value[i] = False
+                            break
 
         expected_X = torch.clone(self.full_X)
         if torch.equal(Y, self.out):
             # TODO: should this be done at every prior layer or should it stop at this layer?
             self.W[self.W > 0] += 1
         else:
-            one_Y_idxs = torch.nonzero((Y == 1) & (Y != self.out)).squeeze(1)
-            for row_idx in one_Y_idxs:
-                update_idxs = [ i for i, (w, v) in enumerate(zip(self.W[row_idx], expected_X)) if w > 0 and v == 0]
-                for update_idx in update_idxs:
-                    self.helper(expected_X, update_idx, self.W[row_idx.item()], can_flip_value, True, False)
+            for s, (y_value, out_value, expected_x_value) in enumerate(zip(Y, self.out, expected_X)):
+                one_Y_idxs = torch.nonzero((y_value == 1) & (y_value != out_value)).squeeze(1)
+                for row_idx in one_Y_idxs:
+                    update_idxs = [ i for i, (w, v) in enumerate(zip(self.W[row_idx], expected_x_value)) if w > 0 and v == 0]
+                    for update_idx in update_idxs:
+                        self.helper(expected_x_value, update_idx, self.W[row_idx.item()], can_flip_value[s], True, False)
 
-            updated_out = self.conjunction_mul(expected_X.unsqueeze(0), self.W).squeeze(0) if not torch.equal(expected_X, self.full_X) else self.out
-            zero_Y_idxs = torch.nonzero((Y == 0) & (Y != updated_out)).squeeze(1)
+            updated_out = self.conjunction_mul(expected_X.unsqueeze(1), self.W) if not torch.equal(expected_X, self.full_X) else self.out
+            for s, (y_value, out_value, expected_x_value) in enumerate(zip(Y, updated_out, expected_X)):
+                zero_Y_idxs = torch.nonzero((y_value == 0) & (y_value != out_value)).squeeze(1)
+                for row_idx in zero_Y_idxs:
+                    candidate_idxs = []
+                    min_W = 0
+                    for j in range(self.in_dim * 2):
+                        W_value = self.W[row_idx.item()][j]
+                        X_value = expected_x_value[j]
+                        if W_value > 0 and X_value == 1:
+                            if W_value < min_W or len(candidate_idxs) == 0:
+                                candidate_idxs = [j]
+                                min_W = W_value
+                            elif W_value == min_W:
+                                candidate_idxs.append(j)
 
-            for row_idx in zero_Y_idxs:
-                candidate_idxs = []
-                min_W = 0
-                for j in range(self.in_dim * 2):
-                    W_value = self.W[row_idx.item()][j]
-                    X_value = expected_X[j]
-                    if W_value > 0 and X_value == 1:
-                        if W_value < min_W or len(candidate_idxs) == 0:
-                            candidate_idxs = [j]
-                            min_W = W_value
-                        elif W_value == min_W:
-                            candidate_idxs.append(j)
-
-                update_idx = random.choice(candidate_idxs)
-                self.helper(expected_X,update_idx, self.W[row_idx.item()], can_flip_value, False, True)
-        return expected_X[:self.in_dim]
+                    update_idx = random.choice(candidate_idxs)
+                    self.helper(expected_x_value,update_idx, self.W[row_idx.item()], can_flip_value[s], False, True)
+        return expected_X[:,:self.in_dim]
 
 class TsetlinMachine:
 
@@ -109,10 +113,10 @@ class TsetlinMachine:
     def forward(self, X):
         X = self.l1.forward(X)
         X = self.l2.forward(X)
-        self.out = X.squeeze(0)
+        self.out = X.squeeze(1)
         return self.out
     
     def update(self, y):
-        y = y.unsqueeze(0)
+        y = y.unsqueeze(1)
         updated_X = self.l2.update(y)
         self.l1.update(updated_X, True)
