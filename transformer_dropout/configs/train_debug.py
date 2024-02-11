c59a903851778e190331b59900c84439ec6b7614
yiphei <yanyiphei@gmail.com>
2024-02-11 13:40:23 -0800

    updated train debug config

---
+++ transformer_dropout/configs/train_debug.py
@@ -0,0 +1,15 @@
+# ModelConfig
+context_size = 100
+n_embed = 100
+n_layer = 3
+n_head = 2
+bias = False
+
+# Training config
+batch_size = 50
+training_steps = 1000
+lr = 3e-4
+
+# Estimation config
+est_interval = 200
+est_steps = 100
