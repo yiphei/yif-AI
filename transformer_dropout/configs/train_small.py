68dc69c18a83f2665343e68ec6ed0201864f8947
yiphei <yanyiphei@gmail.com>
2024-02-11 12:50:02 -0800

    added a training config

---
+++ transformer_dropout/configs/train_small.py
@@ -0,0 +1,15 @@
+# ModelConfig
+context_size = 200
+n_embed = 200
+n_layer = 4
+n_head = 4
+bias = False
+
+# Training config
+batch_size = 70
+training_steps = 5000
+lr = 3e-4
+
+# Estimation config
+est_interval = 500
+est_steps = 200
