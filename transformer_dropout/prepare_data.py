8bb51e37d40365782b28a35512faf2733c9ea16d
yiphei <yanyiphei@gmail.com>
2024-02-12 12:43:46 -0800

    updated training script

---
+++ transformer_dropout/prepare_data.py
@@ -0,0 +1,30 @@
+import os
+import tiktoken
+import numpy as np
+import argparse
+from pathlib import Path
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", type=str)
+    args = parser.parse_args()
+
+    with open(args.file, 'r') as f:
+        data = f.read()
+    n = len(data)
+    train_data = data[:int(n*0.9)]
+    val_data = data[int(n*0.9):]
+
+    # encode with tiktoken gpt2 bpe
+    enc = tiktoken.get_encoding("gpt2")
+    train_ids = enc.encode_ordinary(train_data)
+    val_ids = enc.encode_ordinary(val_data)
+    print(f"train has {len(train_ids):,} tokens")
+    print(f"val has {len(val_ids):,} tokens")
+
+    # export to bin files
+    train_ids = np.array(train_ids, dtype=np.uint16)
+    val_ids = np.array(val_ids, dtype=np.uint16)
+    file_name = Path(args.file).name
+    train_ids.tofile(os.path.join (os.path.dirname(args.file), f'{file_name}_train.bin'))
+    val_ids.tofile(os.path.join(os.path.dirname(args.file), f'{file_name}_val.bin'))
