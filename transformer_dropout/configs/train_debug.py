2da46ad53d7f479b3ab42bd150f9eefa599f4a76
yiphei <yanyiphei@gmail.com>
2024-02-11 19:56:03 -0800

    updated training script and config

--- transformer_dropout/configs/train_debug.py
+++ transformer_dropout/configs/train_debug.py
@@ -6,9 +6,12 @@ n_head = 2
 bias = False
 
 # Training config
-batch_size = 50
+batch_size = 10
 training_steps = 1000
 lr = 3e-4
+warmup_iters = 50
+min_lr = 6e-5
+gradient_accumulation_steps = 7
 
 # Estimation config
 est_interval = 200
