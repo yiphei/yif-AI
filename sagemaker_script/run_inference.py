5c42e3bb8bd97c68bfd2d97a8184db54216ff7ca
yiphei <yanyiphei@gmail.com>
2024-02-17 10:01:48 -0800

    added changes

--- sagemaker_script/run_inference.py
+++ sagemaker_script/run_inference.py
@@ -1,11 +1,13 @@
-from sagemaker.predictor import Predictor
-import numpy as np
-import json
+from sagemaker.pytorch import PyTorchPredictor
 import os
 import boto3
 import sagemaker
 from sagemaker.serializers import JSONSerializer
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--endpoint_name", type=str, required=True)
+args = parser.parse_args()
 
 role = os.getenv("SAGEMAKER_ROLE")
 my_region = "us-east-1"
@@ -21,10 +23,7 @@ sagemaker_session = sagemaker.Session(
     sagemaker_runtime_client=sagemaker_runtime_client,
 )
 
-# Specify your endpoint name
-endpoint_name = 'aaaaaaa'
-# Initialize the predictor
-predictor = Predictor(endpoint_name=endpoint_name, sagemaker_session=sagemaker_session, serializer = JSONSerializer())
+predictor = PyTorchPredictor(endpoint_name=args.endpoint_name, sagemaker_session=sagemaker_session, serializer = JSONSerializer())
 result = predictor.predict({"instances": [1.0, 2.0, 5.0]})
 # The response format depends on the `output_fn` in your inference script
 print(result)
