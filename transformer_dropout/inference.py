c0b47375fd0ea22a59cd60229c68ec1ed0c31c9a
yiphei <yanyiphei@gmail.com>
2024-02-17 11:19:39 -0800

    temp changes

--- transformer_dropout/inference.py
+++ transformer_dropout/inference.py
@@ -25,61 +25,61 @@ def model_fn(model_dir):
     model.to(device)
     return model
 
-def input_fn(request_body, request_content_type):
-    """
-    Deserialize and prepare the prediction input.
-    """
-    print("YIFEII - INPUT_FN")
-    if request_content_type == 'application/json':
-        # Assuming JSON inputs. Adjust as necessary.
-        print("YIFEII-input_fn")
-        print(request_body)
-        input_data = torch.tensor([json.loads(request_body)])
-        print(input_data)
-        return input_data
-    else:
-        # Handle other content-types here or raise an exception
-        raise ValueError(f"Unsupported content type: {request_content_type}")
+# def input_fn(request_body, request_content_type):
+#     """
+#     Deserialize and prepare the prediction input.
+#     """
+#     print("YIFEII - INPUT_FN")
+#     if request_content_type == 'application/json':
+#         # Assuming JSON inputs. Adjust as necessary.
+#         print("YIFEII-input_fn")
+#         print(request_body)
+#         input_data = torch.tensor([json.loads(request_body)])
+#         print(input_data)
+#         return input_data
+#     else:
+#         # Handle other content-types here or raise an exception
+#         raise ValueError(f"Unsupported content type: {request_content_type}")
 
-def predict_fn(input_data, model):
-    """
-    Generate model predictions.
-    """
-    print("YIFEII - PREDICT_FN")
-    dtype = "bfloat16" if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else "float16"
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
-    torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
-    device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
-    ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
-    ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+# def predict_fn(input_data, model):
+#     """
+#     Generate model predictions.
+#     """
+#     print("YIFEII - PREDICT_FN")
+#     dtype = "bfloat16" if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else "float16"
+#     device = "cuda" if torch.cuda.is_available() else "cpu"
+#     torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
+#     torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
+#     device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
+#     ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
+#     ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    enc = tiktoken.get_encoding("gpt2")
-    encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
-    decode = lambda l: enc.decode(l)
+#     device = "cuda" if torch.cuda.is_available() else "cpu"
+#     enc = tiktoken.get_encoding("gpt2")
+#     encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
+#     decode = lambda l: enc.decode(l)
 
-    start_ids = encode(input_data)
-    x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
+#     start_ids = encode(input_data)
+#     x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
 
-    predictions = []
-    # run generation
-    with torch.no_grad():
-        with ctx:
-            for k in range(1):
-                y = model.generate(x, 10000)
-                predictions.append(decode(y[0].tolist()))
+#     predictions = []
+#     # run generation
+#     with torch.no_grad():
+#         with ctx:
+#             for k in range(1):
+#                 y = model.generate(x, 10000)
+#                 predictions.append(decode(y[0].tolist()))
 
-    return predictions
+#     return predictions
 
-def output_fn(prediction_output, accept='application/json'):
-    """
-    Serialize and prepare the prediction output.
-    """
-    print("YIFEII - OUTPUT_FN")
-    if accept == 'application/json':
-        # Convert prediction output to JSON or other formats as needed
-        response_body = json.dumps(prediction_output.tolist())
-        return response_body
-    else:
-        raise ValueError(f"Unsupported accept type: {accept}")
+# def output_fn(prediction_output, accept='application/json'):
+#     """
+#     Serialize and prepare the prediction output.
+#     """
+#     print("YIFEII - OUTPUT_FN")
+#     if accept == 'application/json':
+#         # Convert prediction output to JSON or other formats as needed
+#         response_body = json.dumps(prediction_output.tolist())
+#         return response_body
+#     else:
+#         raise ValueError(f"Unsupported accept type: {accept}")
