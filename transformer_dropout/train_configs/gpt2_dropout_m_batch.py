a8cf193a9b84429e6d50f1f42dc4ec43ea0b8e49
yiphei <yanyiphei@gmail.com>
2024-02-21 13:09:36 -0800

    updated configs

--- transformer_dropout/train_configs/gpt2_dropout_m_batch.py
+++ transformer_dropout/train_configs/gpt2_dropout_m_batch.py
@@ -10,14 +10,14 @@ USE_LEARNED_DROPOUT = True
 
 # Training config
 BATCH_SIZE = 7  # workst with 6 and 7, but 12 and 8 failed with 2 ml.p4d.24 instances
-TRAIN_STEPS = 600000
+TRAIN_STEPS = 22000
 LR = 6e-4
 WARMUP_ITERS = 2000
 MIN_LR = 6e-5
 GRADIENT_ACCUMULATION_STEPS = (
     2 * 8
 )  # gpt2_baseline has 5*8 but this uses 2*8 because it has bigger memory footprint
-LR_DECAY_ITERS = 600000
+LR_DECAY_ITERS = 22000
 
 # Estimation config
 EST_INTERVAL = 1000
--- transformer_dropout/train_configs/gpt2_dropout_m_batch_lambdas.py
+++ transformer_dropout/train_configs/gpt2_dropout_m_batch_lambdas.py
@@ -12,14 +12,14 @@ DROPOUT_L1_NORM_LAMBDA = 2
 
 # Training config
 BATCH_SIZE = 7  # workst with 6 and 7, but 12 and 8 failed with 2 ml.p4d.24 instances
-TRAIN_STEPS = 600000
+TRAIN_STEPS = 22000
 LR = 6e-4
 WARMUP_ITERS = 2000
 MIN_LR = 6e-5
 GRADIENT_ACCUMULATION_STEPS = (
     2 * 8
 )  # gpt2_baseline has 5*8 but this uses 2*8 because it has bigger memory footprint
-LR_DECAY_ITERS = 600000
+LR_DECAY_ITERS = 22000
 
 # Estimation config
 EST_INTERVAL = 1000
