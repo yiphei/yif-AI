a7b30b4982b6375f60698ac068610f1e8b396e90
yiphei <yanyiphei@gmail.com>
2024-02-25 13:56:14 -0800

    linting

--- transformer_dropout/train_configs/train_debug_dropout_lambdas_annealing.py
+++ transformer_dropout/train_configs/train_debug_dropout_lambdas_annealing.py
@@ -5,12 +5,22 @@ N_LAYER = 3
 N_HEAD = 2
 BIAS = False
 USE_LEARNED_DROPOUT = True
-LEARNED_DROPOUT_CONFIG = {"use_dropout_entropy_in_loss": True, 
-                  "use_dropout_l1_norm_in_loss": True, 
-                  "use_canonical_entropy": True, 
-                  "use_detached_x_in_dropout_mask": True,
-                  "dropout_entropy_lambda": {"min_lambda": 0.1, "max_lambda": 2, "coefficient": 0.001},
-                   "dropout_l1_norm_lambda": {"min_lambda": 0.1, "max_lambda": 2, "coefficient": 0.001} }
+LEARNED_DROPOUT_CONFIG = {
+    "use_dropout_entropy_in_loss": True,
+    "use_dropout_l1_norm_in_loss": True,
+    "use_canonical_entropy": True,
+    "use_detached_x_in_dropout_mask": True,
+    "dropout_entropy_lambda": {
+        "min_lambda": 0.1,
+        "max_lambda": 2,
+        "coefficient": 0.001,
+    },
+    "dropout_l1_norm_lambda": {
+        "min_lambda": 0.1,
+        "max_lambda": 2,
+        "coefficient": 0.001,
+    },
+}
 
 # Training config
 BATCH_SIZE = 10
