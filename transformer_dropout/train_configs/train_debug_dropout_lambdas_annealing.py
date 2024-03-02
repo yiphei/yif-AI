1116b03d1864593bd76c5d9803b50c5c7f14683f
yiphei <yanyiphei@gmail.com>
2024-03-01 19:37:44 -0800

    refactored dropout config into separate dataclass

--- transformer_dropout/train_configs/train_debug_dropout_lambdas_annealing.py
+++ transformer_dropout/train_configs/train_debug_dropout_lambdas_annealing.py
@@ -6,13 +6,20 @@ N_HEAD = 2
 BIAS = False
 USE_LEARNED_DROPOUT = True
 LEARNED_DROPOUT_CONFIG = {
+    "use_sigmoid_on_dropout_mask": False,
     "use_dropout_entropy_in_loss": True,
     "use_dropout_l1_norm_in_loss": False,
-    "a_param_mean": 100000,
-    "a_param_std": 0.02,
-    "A_optimizer_type": "ADAMW",
-    "B_optimizer_type": "ADAMW",
-    "A_lr": 1000,
+    "A_param_config": {
+        "init_mean": 100000,
+        "init_std": 10,
+        "optimizer_type": "ADAMW",
+        "lr": 1,
+    },
+    "B_param_config": {
+        "init_mean": 0.0,
+        "init_std": 0.2,
+        "optimizer_type": "ADAMW",
+    }
 }
 
 # Training config
@@ -27,3 +34,4 @@ LR_DECAY_ITERS = 500
 # Estimation config
 EST_INTERVAL = 100
 EST_STEPS = 20
+DEVICE = "mps"
