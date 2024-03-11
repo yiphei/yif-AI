8178eb9d5ee4a84dd6931d18bf33c3ae7e4955c3
yiphei <yanyiphei@gmail.com>
2024-03-11 17:32:07 +0900

    changed lamda script

--- lambda.sh
+++ lambda.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <API_KEY> <AWS_ACCESS_KEY> <AWS_SECRET_KEY>"
+    exit 1
+fi
+
+# Assign the arguments to variables
+API_KEY=$1
+AWS_ACCESS_KEY=$2
+AWS_SECRET_KEY=$3
+
 ADDRESS_FILE="addresses.txt"
 
 process_address() {
     address=$1
+    api_key=$2
+    aws_access_key=$3
+    aws_secret_key=$4
     echo "Starting process for $address"
     
     # Sync files
@@ -14,7 +27,8 @@ process_address() {
     ssh -i ~/Downloads/lambda.pem -o StrictHostKeyChecking=no "$address" <<'EOF'
     tmux new-session -d -s mySession 'cd yif-AI && pip install -r requirements.txt'
     tmux send-keys -t mySession 'pip install --upgrade pyOpenSSL cryptography boto3 botocore' C-m
-    tmux send-keys -t mySession 'torchrun --standalone --nproc_per_node=1 transformer_embed/training_script.py --config_file transformer_embed/train_configs/harry_potter_new.py --train datasets/full_harry_potter/ --platform_type LAMBDA --aws_access_key_id YOUR_ACCESS_KEY --aws_secret_access_key YOUR_SECRET_KEY' C-m
+    tmux send-keys -t mySession "export WANDB_API_KEY='${api_key}'" C-m
+    tmux send-keys -t mySession 'torchrun --standalone --nproc_per_node=1 transformer_dropout/training_script.py --config_file transformer_dropout/train_configs/harry_potter_baseline.py --train datasets/full_harry_potter/ --platform_type LAMBDA --aws_access_key_id '${aws_access_key}' --aws_secret_access_key '${aws_secret_key}'' C-m
 EOF
 
     echo "Commands executed in tmux session for $address"
@@ -23,7 +37,7 @@ EOF
 # Read each address from the file and process it in the background
 while IFS= read -r address
 do
-    process_address "$address" &
+    process_address "$address" "$API_KEY" "$AWS_ACCESS_KEY" "$AWS_SECRET_KEY" &
 done < "$ADDRESS_FILE"
 
 # Wait for all background processes to finish
