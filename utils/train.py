8d9660ab50f637e95d8f137e6e244e8637554e68
yiphei <yanyiphei@gmail.com>
2024-03-17 20:08:58 -0700

    linting

--- utils/train.py
+++ utils/train.py
@@ -135,7 +135,7 @@ class TrainConfig:
             assert hasattr(self, k)
 
             # for some reason, sometimes wandb does not convert this to a float
-            if k in ['lr', 'min_lr']:
+            if k in ["lr", "min_lr"]:
                 v = float(v)
             setattr(self, k, v)
 
