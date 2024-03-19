08796c00c7a197407e5f9e341dd89ed9fc9974f6
yiphei <yanyiphei@gmail.com>
2024-03-18 21:40:30 -0700

    updated requirements.txt

--- run_training_script.sh
+++ run_training_script.sh
@@ -30,10 +30,12 @@ process_address() {
     tmux send-keys -t mySession "cd yif-AI" C-m
     tmux send-keys -t mySession "pip install -r requirements.txt" C-m
     tmux send-keys -t mySession "pip install --upgrade pyOpenSSL cryptography boto3 botocore" C-m
-    tmux send-keys -t mySession "mkdir datasets/openweb/" C-m
-    tmux send-keys -t mySession "export AWS_ACCESS_KEY_ID=\"${aws_access_key}\"" C-m
-    tmux send-keys -t mySession "export AWS_SECRET_ACCESS_KEY=\"${aws_secret_key}\"" C-m
-    tmux send-keys -t mySession "aws s3 sync s3://dropout-transformer/datasets/openweb/ datasets/openweb/" C-m
+    if [[ "${sync_s3}" == "true" ]]; then
+        tmux send-keys -t mySession "mkdir datasets/openweb/" C-m
+        tmux send-keys -t mySession "export AWS_ACCESS_KEY_ID=\"${aws_access_key}\"" C-m
+        tmux send-keys -t mySession "export AWS_SECRET_ACCESS_KEY=\"${aws_secret_key}\"" C-m
+        tmux send-keys -t mySession "aws s3 sync s3://dropout-transformer/datasets/openweb/ datasets/openweb/" C-m
+    fi
     tmux send-keys -t mySession "export WANDB_API_KEY='${api_key}'" C-m
     tmux send-keys -t mySession "torchrun --standalone --nproc_per_node=1 -m transformer_dropout.training_script --config_file transformer_dropout/train_configs/harry_potter_baseline.py --train datasets/full_harry_potter/ --platform_type LAMBDA --aws_access_key_id ${aws_access_key} --aws_secret_access_key ${aws_secret_key}" C-m
     exit
