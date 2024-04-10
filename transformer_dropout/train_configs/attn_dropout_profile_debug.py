4dc1e01baf9cecd5f8554b7dd55603135d655525
yiphei <yanyiphei@gmail.com>
2024-04-10 16:45:28 -0700

    added new train config

---
+++ transformer_dropout/train_configs/attn_dropout_profile_debug.py
@@ -0,0 +1,32 @@
+# ModelConfig
+CONTEXT_SIZE = 200
+N_EMBED = 200
+N_LAYER = 4
+N_HEAD = 4
+BIAS = False
+USE_LEARNED_DROPOUT = True
+DROPOUT_RATE = 0.3
+LEARNED_DROPOUT_LAYERS = 1
+LEARNED_DROPOUT_CONFIG = {
+    "use_dropout_entropy_in_loss": True,
+    "use_dropout_l1_norm_in_loss": False,
+    "use_bias": False,
+    "use_detached_x_in_dropout_mask": False,
+    "profile_dropout_mask": True,
+    "dropout_entropy_lambda": {
+        "coefficient": 0.0006,
+    },
+}
+
+# Training config
+BATCH_SIZE = 50  # 50 when run on 1x A10
+TRAIN_STEPS = 3000
+LR = 3e-4
+WARMUP_ITERS = 100
+MIN_LR = 3e-5
+GRADIENT_ACCUMULATION_STEPS = 16
+LR_DECAY_ITERS = 3000
+
+# Estimation config
+EST_INTERVAL = 200
+EST_STEPS = 100
