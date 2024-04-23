9d7e30dbc328d451d87985dea11847701668942d
yiphei <yanyiphei@gmail.com>
2024-04-23 12:54:47 -0700

    reverted unused changes

--- transformer_dropout/train_configs/dropout_attn_profile_metric.py
+++ transformer_dropout/train_configs/dropout_attn_profile_metric.py
@@ -1,10 +1,10 @@
 import torch
 
 # ModelConfig
-CONTEXT_SIZE = 50
-N_EMBED = 50
-N_LAYER = 2
-N_HEAD = 2
+CONTEXT_SIZE = 100
+N_EMBED = 90
+N_LAYER = 3
+N_HEAD = 3
 BIAS = False
 USE_LEARNED_DROPOUT = True
 DROPOUT_RATE = 0.3
@@ -13,12 +13,12 @@ LEARNED_DROPOUT_CONFIG = {
     "use_dropout_entropy_in_loss": True,
     "use_dropout_l1_norm_in_loss": False,
     "use_bias": False,
-    "shift_init": 1.2,
+    "shift_init": torch.pi / 2,
     "softmax_dim": 2,
-    "n_heads": 2,
+    "n_heads": 3,
     "use_canonical_entropy": False,
     "rounding_type": 1,
-    "sigmoid_slope": 30,
+    "sigmoid_slope": 100,
     "use_detached_x_in_dropout_mask": False,
     "profile_dropout_mask": False,
     "dropout_entropy_lambda": {
@@ -27,14 +27,13 @@ LEARNED_DROPOUT_CONFIG = {
 }
 
 # Training config
-BATCH_SIZE = 10  # 50 when run on 1x A10
+BATCH_SIZE = 15  # 50 when run on 1x A10
 TRAIN_STEPS = 1000
 LR = 3e-4
 WARMUP_ITERS = 50
 MIN_LR = 3e-5
 GRADIENT_ACCUMULATION_STEPS = 16
 LR_DECAY_ITERS = 1000
-
 # Estimation config
 EST_INTERVAL = 100
 EST_STEPS = 20
