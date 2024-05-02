a081e8123c3ad8a5820b2fc5e63b6f7717bbab7e
yiphei <yanyiphei@gmail.com>
2024-05-02 11:51:01 -0700

    updated config

--- transformer_dropout/train_configs/big_new.py
+++ transformer_dropout/train_configs/big_new.py
@@ -3,14 +3,14 @@ import torch
 # ModelConfig
 CONTEXT_SIZE = 100
 N_EMBED = 150
-N_LAYER = 5
+N_LAYER = 6
 N_HEAD = 5
 BIAS = False
 USE_LEARNED_DROPOUT = True
 DROPOUT_RATE = 0.3
 LEARNED_DROPOUT_CONFIG = {
     "start_layer": 1,
-    "end_layer": 5,
+    "end_layer": 6,
     "use_bias": False,
     "n_heads": 5,
     "profile_dropout_mask": False,
