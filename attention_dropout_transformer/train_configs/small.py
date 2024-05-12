ff6eff721eb07032b8924eb06b3756ad0d5ee554
yifei yan <yanyiphei@gmail.com>
2024-05-12 12:07:24 -0700

    more refactor

--- attention_dropout_transformer/train_configs/small.py
+++ attention_dropout_transformer/train_configs/small.py
@@ -21,7 +21,7 @@ ATTENTION_DROPOUT_CONFIG = {
     "n_head": 3,
     "use_canonical_entropy": False,
     "rounding_type": 1,
-    "sigmoid_slope": 100,
+    "sigmoid_scale": 100,
     "use_detached_x_in_dropout_mask": False,
 }
 
