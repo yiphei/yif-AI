05cd496dd348dfac049d069af3193a5ce90b2ab5
yifei yan <yanyiphei@gmail.com>
2024-05-13 12:53:52 -0700

    updated script

--- run_training_script.sh
+++ run_training_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -lt 4 ] || [ "$#" -gt 5 ]; then
-    echo "Usage: $0 <ADDRESS_FILE_PATH> <API_KEY> <AWS_ACCESS_KEY> <AWS_SECRET_KEY> <SYNC_S3>"
+if [ "$#" -lt 5 ] || [ "$#" -gt 6 ]; then
+    echo "Usage: $0 <ADDRESS_FILE_PATH> <API_KEY> <AWS_ACCESS_KEY> <AWS_SECRET_KEY> <SYNC_S3> <SYNC_FILES>"
     exit 1
 fi
 
@@ -11,6 +11,7 @@ API_KEY=$2
 AWS_ACCESS_KEY=$3
 AWS_SECRET_KEY=$4
 SYNC_S3=${5:-false}
+SYNC_FILES=${6:-true}
 
 process_address() {
     address=$1
@@ -18,11 +19,14 @@ process_address() {
     aws_access_key=$3
     aws_secret_key=$4
     sync_s3=$5
+    sync_files=$6
     echo "Starting process for $address"
 
-    # Sync files
-    echo "Syncing files to $address"
-    git ls-files | rsync -avz --files-from=- -e "ssh -i ~/Downloads/lambda.pem -o StrictHostKeyChecking=no" ./ "$address":~/yif-AI/
+    if [[ "$(echo "$sync_files" | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
+        # Sync files
+        echo "Syncing files to $address"
+        git ls-files | rsync -avz --files-from=- -e "ssh -i ~/Downloads/lambda.pem -o StrictHostKeyChecking=no" ./ "$address":~/yif-AI/
+    fi
 
     # SSH into the server, start a tmux session, and run the commands
     ssh -i ~/Downloads/lambda.pem -o StrictHostKeyChecking=no "$address" <<EOF
@@ -50,7 +54,7 @@ while IFS=' ' read -r ssh_cmd address _ || [[ -n "$address" ]]; do
     if [[ -z "$address" ]]; then
         continue
     fi
-    process_address "$address" "$API_KEY" "$AWS_ACCESS_KEY" "$AWS_SECRET_KEY" "$SYNC_S3"
+    process_address "$address" "$API_KEY" "$AWS_ACCESS_KEY" "$AWS_SECRET_KEY" "$SYNC_S3" "$SYNC_FILES"
 done < "$ADDRESS_FILE"
 
 # Wait for all background processes to finish
