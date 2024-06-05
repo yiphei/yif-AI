2d64912deeb4113031b76ea5bca36fbcba7ba1a1
yifei yan <yanyiphei@gmail.com>
2024-06-04 21:12:44 -0700

    added changes

---
+++ future_attention_transformer/train_configs/test.py
@@ -0,0 +1,21 @@
+# ModelConfig
+CONTEXT_SIZE = 3
+N_EMBED = 4
+N_LAYER = 3
+N_HEAD = 2
+USE_BIAS = False
+DROPOUT_RATE = 0
+FUTURE_DIM = 2
+
+# Training config
+BATCH_SIZE = 2  # 50 when run on 1x A10
+TRAIN_STEPS = 500
+LR = 3e-4
+WARMUP_ITERS = 50
+MIN_LR = 3e-5
+GRADIENT_ACCUMULATION_STEPS = 1
+LR_DECAY_ITERS = 500
+
+# Estimation config
+EST_INTERVAL = 100
+EST_STEPS = 20
