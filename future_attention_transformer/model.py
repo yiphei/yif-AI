1e05967277d317490543f5e9a07490ccd1b5a0d6
yifei yan <yanyiphei@gmail.com>
2024-06-05 16:33:22 -0700

    added changse

--- future_attention_transformer/model.py
+++ future_attention_transformer/model.py
@@ -59,8 +59,6 @@ class ModelConfig(BaseModelConfig):
         if self.end_layer > self.n_layer or self.end_layer < 1:
             raise ValueError("end_layer must be <= n_layer and >= 1")
 
-        assert 1 <= self.future_dim <= (self.context_size - 1)
-
         assert self.future_x_loss_coeff > 0
 
         if self.detach_future_x is None:
