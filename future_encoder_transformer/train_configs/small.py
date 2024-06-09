4497213c72447fbe40e0f3355a7f36dcc864fa58
yifei yan <yanyiphei@gmail.com>
2024-06-09 10:56:08 -0700

    added changes

--- future_encoder_transformer/train_configs/small.py
+++ future_encoder_transformer/train_configs/small.py
@@ -6,7 +6,7 @@ N_HEAD = 3
 USE_BIAS = False
 DROPOUT_RATE = 0
 future_context_size = 5
-include_past = True
+include_present_context_in_embed = True
 
 # Training config
 BATCH_SIZE = 15  # 50 when run on 1x A10
