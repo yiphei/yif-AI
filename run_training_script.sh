ad1e16a709e477d5a0debc0b96ffedcea4c7ebd8
yifei yan <yanyiphei@gmail.com>
2024-06-10 14:21:57 -0700

    changed bash script

--- run_training_script.sh
+++ run_training_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -lt 5 ] || [ "$#" -gt 6 ]; then
-    echo "Usage: $0 <ADDRESS_FILE_PATH> <API_KEY> <AWS_ACCESS_KEY> <AWS_SECRET_KEY> <SYNC_S3> <SYNC_FILES>"
+if [ "$#" -lt 5 ] || [ "$#" -gt 7 ]; then
+    echo "Usage: $0 <ADDRESS_FILE_PATH> <API_KEY> <AWS_ACCESS_KEY> <AWS_SECRET_KEY> <SYNC_S3> <SYNC_FILES> <GPU_PROCESSES>"
     exit 1
 fi
 
@@ -12,6 +12,7 @@ AWS_ACCESS_KEY=$3
 AWS_SECRET_KEY=$4
 SYNC_S3=${5:-false}
 SYNC_FILES=${6:-true}
+GPU_PROCESSES=${7:-1}
 
 process_address() {
     address=$1
@@ -20,6 +21,7 @@ process_address() {
     aws_secret_key=$4
     sync_s3=$5
     sync_files=$6
+    gpu_processes=$7
     echo "Starting process for $address"
 
     if [[ "$(echo "$sync_files" | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
@@ -41,8 +43,21 @@ process_address() {
         tmux send-keys -t mySession "export AWS_SECRET_ACCESS_KEY=\"${aws_secret_key}\"" C-m
         tmux send-keys -t mySession "aws s3 sync s3://dropout-transformer/datasets/openweb/ datasets/openweb/" C-m
     fi
-    tmux send-keys -t mySession "export WANDB_API_KEY='${api_key}'" C-m
-    tmux send-keys -t mySession "torchrun --standalone --nproc_per_node=1 -m future_encoder_transformer.training_script --config_file future_encoder_transformer/train_configs/small.py --train datasets/wikipedia/ --platform_type LAMBDA --aws_access_key_id ${aws_access_key} --aws_secret_access_key ${aws_secret_key} --sweep_id a5e8ggsj --sweep_count 1 --sync_profile_live True" C-m
+
+    export_cmd = "export WANDB_API_KEY='${api_key}'"
+    torch_cmd = "torchrun --standalone --nproc_per_node=1 -m future_encoder_transformer.training_script --config_file future_encoder_transformer/train_configs/small.py --train datasets/wikipedia/ --platform_type LAMBDA --aws_access_key_id ${aws_access_key} --aws_secret_access_key ${aws_secret_key} --sweep_id a5e8ggsj --sweep_count 1 --sync_profile_live True"
+
+    if [ $gpu_processes -gt 1 ]; then
+        for i in \$(seq 0 $((gpu_processes-1))); do
+            session_name="mySession\$i"
+            tmux new-session -d -s \$session_name /bin/bash
+            tmux send-keys -t \$session_name "$export_cmd" C-m
+            tmux send-keys -t \$session_name "CUDA_VISIBLE_DEVICES=\$i $torch_cmd" C-m
+        done
+    else
+        tmux send-keys -t mySession "$export_cmd" C-m
+        tmux send-keys -t mySession "$torch_cmd" C-m
+    fi
     exit
 EOF
 
@@ -54,7 +69,7 @@ while IFS=' ' read -r ssh_cmd address _ || [[ -n "$address" ]]; do
     if [[ -z "$address" ]]; then
         continue
     fi
-    process_address "$address" "$API_KEY" "$AWS_ACCESS_KEY" "$AWS_SECRET_KEY" "$SYNC_S3" "$SYNC_FILES"
+    process_address "$address" "$API_KEY" "$AWS_ACCESS_KEY" "$AWS_SECRET_KEY" "$SYNC_S3" "$SYNC_FILES" "$GPU_PROCESSES"
 done < "$ADDRESS_FILE"
 
 # Wait for all background processes to finish
