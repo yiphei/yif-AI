2f3329613479c024304b130c266d16e464d187ef
yifei yan <yanyiphei@gmail.com>
2024-06-26 10:49:03 -0700

    linting

--- autoregressive_encoder_decoder_transformer/training_script.py
+++ autoregressive_encoder_decoder_transformer/training_script.py
@@ -1,7 +1,8 @@
 from utils.train import train
 
 try:
-    from autoregressive_encoder_decoder_transformer.model import AutoregressiveEncoderDecoderTransformer
+    from autoregressive_encoder_decoder_transformer.model import \
+        AutoregressiveEncoderDecoderTransformer
 except ImportError:
     # I only upload the direct parent module to sagemaker, so I need a different import path
     from model import AutoregressiveEncoderDecoderTransformer
