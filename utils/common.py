343eb3e862621afa2aaadb2506f646cee29c92f0
yifei yan <yanyiphei@gmail.com>
2024-07-09 15:14:22 -0700

    added changes

--- utils/common.py
+++ utils/common.py
@@ -6,7 +6,7 @@ import numpy as np
 import torch
 from typing import get_type_hints
 
-class AutoMappedEnum(StrEnum):
+class IntMappedEnum(StrEnum):
     @classmethod
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
@@ -17,23 +17,7 @@ class AutoMappedEnum(StrEnum):
     def from_int(cls, value: int):
         return cls.int_mapping[value]
 
-# class EnumFieldDescriptor:
-#     def __init__(self, enum_class: Type[AutoMappedEnum]):
-#         self.enum_class = enum_class
-
-#     def __set_name__(self, owner, name):
-#         self.name = name
-
-#     def __set__(self, instance, value):
-#         if isinstance(value, int):
-#             value = self.enum_class.from_int(value)
-#         elif isinstance(value, str):
-#             value = self.enum_class(value)
-#         elif not isinstance(value, self.enum_class):
-#             raise ValueError(f"Value must be an integer, string, or an instance of {self.enum_class}")
-#         instance.__dict__[self.name] = value
-
-def adapted_dataclass(_cls=None, **kwargs):
+def custom_dataclass(_cls=None, **kwargs):
     def wrap(cls):
         sub_post_init = getattr(cls, '__post_init__', None)
         def enum_post_init(self):
@@ -42,7 +26,7 @@ def adapted_dataclass(_cls=None, **kwargs):
             for field in fields(self):
                 hint = hints[field.name]
                 
-                if isinstance(hint, type) and issubclass(hint, AutoMappedEnum):
+                if isinstance(hint, type) and issubclass(hint, IntMappedEnum):
                     value = getattr(self, field.name)
                     if isinstance(value, int):
                         setattr(self, field.name, hint.from_int(value))
