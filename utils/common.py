54a223027bcab8e08793e40a04db0521bdbf38f0
yifei yan <yanyiphei@gmail.com>
2024-07-09 15:08:40 -0700

    added changes

--- utils/common.py
+++ utils/common.py
@@ -1,10 +1,10 @@
 import random
 from contextlib import contextmanager, nullcontext
 from enum import StrEnum
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 import numpy as np
 import torch
-from typing import Type, get_type_hints
+from typing import get_type_hints
 
 class AutoMappedEnum(StrEnum):
     @classmethod
@@ -17,33 +17,43 @@ class AutoMappedEnum(StrEnum):
     def from_int(cls, value: int):
         return cls.int_mapping[value]
 
-class EnumFieldDescriptor:
-    def __init__(self, enum_class: Type[AutoMappedEnum]):
-        self.enum_class = enum_class
+# class EnumFieldDescriptor:
+#     def __init__(self, enum_class: Type[AutoMappedEnum]):
+#         self.enum_class = enum_class
 
-    def __set_name__(self, owner, name):
-        self.name = name
+#     def __set_name__(self, owner, name):
+#         self.name = name
 
-    def __set__(self, instance, value):
-        if isinstance(value, int):
-            value = self.enum_class.from_int(value)
-        elif isinstance(value, str):
-            value = self.enum_class(value)
-        elif not isinstance(value, self.enum_class):
-            raise ValueError(f"Value must be an integer, string, or an instance of {self.enum_class}")
-        instance.__dict__[self.name] = value
+#     def __set__(self, instance, value):
+#         if isinstance(value, int):
+#             value = self.enum_class.from_int(value)
+#         elif isinstance(value, str):
+#             value = self.enum_class(value)
+#         elif not isinstance(value, self.enum_class):
+#             raise ValueError(f"Value must be an integer, string, or an instance of {self.enum_class}")
+#         instance.__dict__[self.name] = value
 
 def adapted_dataclass(_cls=None, **kwargs):
     def wrap(cls):
-        cls = dataclass(cls, **kwargs)
-        hints = get_type_hints(cls)
-        
-        for name, hint in hints.items():
-            if isinstance(hint, type) and issubclass(hint, AutoMappedEnum):
-                descriptor = EnumFieldDescriptor(hint)
-                descriptor.__set_name__(cls, name)
-                setattr(cls, name, descriptor)
-        
+        sub_post_init = getattr(cls, '__post_init__', None)
+        def enum_post_init(self):
+            hints = get_type_hints(cls)
+
+            for field in fields(self):
+                hint = hints[field.name]
+                
+                if isinstance(hint, type) and issubclass(hint, AutoMappedEnum):
+                    value = getattr(self, field.name)
+                    if isinstance(value, int):
+                        setattr(self, field.name, hint.from_int(value))
+                    elif isinstance(value, str):
+                        setattr(self, field.name, hint(value))
+            
+            if sub_post_init:
+                sub_post_init(self)
+
+        setattr(cls, '__post_init__', enum_post_init)
+        cls = dataclass(cls, **kwargs)        
         return cls
 
     if _cls is None:
