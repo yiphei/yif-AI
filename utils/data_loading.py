117b3b40198455e67828ff19929025d183572244
yifei yan <yanyiphei@gmail.com>
2024-07-13 13:34:07 -0700

    added changes

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -49,16 +49,13 @@ class MapLocalDataset(Dataset):
         return len(self.data) - self.context_size - 1
 
     def __getitems__(self, idxs):
-        elements = []
-        for idx in idxs:
-            x = torch.from_numpy(
-                (self.data[idx : idx + self.context_size]).astype(np.int64)
-            )
-            y = torch.from_numpy(
-                (self.data[idx + 1 : idx + self.context_size + 1]).astype(np.int64)
-            )
-            elements.append((x, y))
-        return elements
+        x = torch.stack([torch.from_numpy(
+            (self.data[idx : idx + self.context_size]).astype(np.int64)
+        ) for idx in idxs])
+        y = torch.stack([torch.from_numpy(
+            (self.data[idx + 1 : idx + self.context_size + 1]).astype(np.int64)
+        ) for idx in idxs])
+        return x, y
 
     @classmethod
     def create_with_distributed_sampler(
