738e7daf26f955f1720bd8c83317996f0de386b7
yifei yan <yanyiphei@gmail.com>
2024-07-19 14:35:44 -0700

    linting

--- learned_dropout/model.py
+++ learned_dropout/model.py
@@ -499,7 +499,10 @@ class LearnedDropoutTransformer(BaseModel):
         ]:
 
             transformed = self.embed_transform(embed)
-            if self.config.learned_dropout_config.dropout_input_type == DropoutInputType.EMBED_WITH_TRANSFORMATION_AND_RES:
+            if (
+                self.config.learned_dropout_config.dropout_input_type
+                == DropoutInputType.EMBED_WITH_TRANSFORMATION_AND_RES
+            ):
                 embed = embed + transformed
             else:
                 embed = transformed
